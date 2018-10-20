266e717f34fc78eefa7e89b986ed26272fde7fd8
Richard Henderson <richard.henderson@linaro.org>
2018-10-20 13:54:46 -0700

    cputlb: Move cpu->pending_tlb_flush to env->tlb_pending_flush
    Protect it with the tlb_lock instead of using atomics.
    The move puts it in or near the same cacheline as the lock;
    using the lock means we don't need a second atomic operation
    in order to perform the update.  Which makes it cheap to also
    update tlb_pending_flush in tlb_flush_by_mmuidx_async_work.
    
    Signed-off-by: Richard Henderson <richard.henderson@linaro.org>

--- include/exec/cpu-defs.h
+++ include/exec/cpu-defs.h
@@ -141,10 +141,16 @@ typedef struct CPUIOTLBEntry {
     MemTxAttrs attrs;
 } CPUIOTLBEntry;
 
+/*
+ * The meaning of the MMU modes is defined in the target code.
+ * tlb_lock serializes updates to tlb_table and tlb_v_table,
+ * and tlb_pending_flush.
+ */
 #define CPU_COMMON_TLB \
     /* The meaning of the MMU modes is defined in the target code. */   \
     /* tlb_lock serializes updates to tlb_table and tlb_v_table */      \
     QemuSpin tlb_lock;                                                  \
+    uint16_t tlb_pending_flush;                                         \
     CPUTLBEntry tlb_table[NB_MMU_MODES][CPU_TLB_SIZE];                  \
     CPUTLBEntry tlb_v_table[NB_MMU_MODES][CPU_VTLB_SIZE];               \
     CPUIOTLBEntry iotlb[NB_MMU_MODES][CPU_TLB_SIZE];                    \
